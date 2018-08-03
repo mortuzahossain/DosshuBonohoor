e874e391be28d42ae2e06801096a19c43b6d970b
Mortuza Hossain <mortuzahossain1997@gmail.com>
2018-08-04 05:58:27 +0600

    Done viewing pdf file.

--- app/src/main/java/com/wordpress/mortuza99/dosshubonohoor/MYPDFReader.java
+++ app/src/main/java/com/wordpress/mortuza99/dosshubonohoor/MYPDFReader.java
@@ -1,7 +1,13 @@
 package com.wordpress.mortuza99.dosshubonohoor;
 
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.github.barteksc.pdfviewer.PDFView;
+
+import java.io.File;
+
 
 public class MYPDFReader extends AppCompatActivity {
 
@@ -9,5 +15,18 @@ public class MYPDFReader extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mypdfreader);
+
+        PDFView pdfView = findViewById(R.id.pdfView);
+
+        if (getIntent().getStringExtra("FILENAME") == null) {
+            String FILENAME = getIntent().getStringExtra("FILENAME");
+            File file = new File(Environment.getExternalStorageDirectory().getAbsolutePath(), Environment.DIRECTORY_DOWNLOADS + "/Books/" + FILENAME);
+            pdfView.fromFile(file)
+                    .load();
+        } else {
+            String URL = getIntent().getStringExtra("URL");
+            // TODO: LOAD USING INTERNET
+        }
+
     }
 }
