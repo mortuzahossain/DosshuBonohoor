1ff840ad9dc5fe9f86ce10ed7345966f6164f487
Mortuza Hossain <mortuzahossain1997@gmail.com>
2018-08-04 04:58:47 +0600

    Added Bottom Navigation Drawer.

--- app/src/main/java/com/wordpress/mortuza99/dosshubonohoor/MainActivity.java
+++ app/src/main/java/com/wordpress/mortuza99/dosshubonohoor/MainActivity.java
@@ -7,15 +7,21 @@ import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.FrameLayout;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.wordpress.mortuza99.dosshubonohoor.fragments.HomeFragment;
+import com.wordpress.mortuza99.dosshubonohoor.fragments.LibraryFragment;
 
 import java.util.Arrays;
 
@@ -25,14 +31,20 @@ public class MainActivity extends AppCompatActivity {
     private FirebaseAuth.AuthStateListener authStateListener;
     public static final int RC_SIGN_IN = 1;
 
+    private BottomNavigationView mNavigationView;
+    private FrameLayout mMaineFrame;
+
+    private HomeFragment homeFragment;
+    private LibraryFragment libraryFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        firebaseAuth = FirebaseAuth.getInstance();
+        ViewInitialization();
 
-        if (isOnline()) {
+        if (IsOnline()) {
             authStateListener = new FirebaseAuth.AuthStateListener() {
                 @Override
                 public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
@@ -54,17 +66,49 @@ public class MainActivity extends AppCompatActivity {
                 }
             };
         } else {
-            showWorning();
+            ShowWorning();
         }
 
     }
 
-    private void UserSignedIn() {
-        // TODO : DO ALL THE STUFF AFTER USER LOGIN
-        Toast.makeText(getApplicationContext(), "LOGID IN", Toast.LENGTH_LONG).show();
+    private void setFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.mainframe, fragment);
+        fragmentTransaction.commit();
     }
 
-    private void showWorning() {
+    private void ViewInitialization() {
+        firebaseAuth = FirebaseAuth.getInstance();
+        mMaineFrame = findViewById(R.id.mainframe);
+        mNavigationView = findViewById(R.id.navbottom);
+
+        homeFragment = new HomeFragment();
+        libraryFragment = new LibraryFragment();
+    }
+
+    private void UserSignedIn() {
+
+        setFragment(homeFragment);
+
+        mNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.navhome:
+                        setFragment(homeFragment);
+                        return true;
+                    case R.id.navlibrary:
+                        setFragment(libraryFragment);
+                        return true;
+                    default:
+                        return false;
+                }
+            }
+        });
+
+    }
+
+    private void ShowWorning() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("Warning !!!");
         builder.setMessage("Please connect your phone with internet.");
@@ -79,7 +123,7 @@ public class MainActivity extends AppCompatActivity {
         warning.show();
     }
 
-    private boolean isOnline() {
+    private boolean IsOnline() {
         ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo netInfo = cm.getActiveNetworkInfo();
         if (netInfo != null && netInfo.isConnectedOrConnecting()) {
@@ -115,4 +159,5 @@ public class MainActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
 }
